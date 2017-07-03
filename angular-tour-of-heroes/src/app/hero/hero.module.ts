cac6d6964d221bee65fd6808795963f80761b760
Jean Flores <jeansflores@outlook.com>
2017-07-02 23:28:48 -0300

    Iniciando Tour of Heroes Angular

---
+++ angular-tour-of-heroes/src/app/hero/hero.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
+
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroListComponent } from './hero-list.component';
+import { HeroService } from './hero.service';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule
+    ],
+    declarations: [
+        HeroDetailComponent,
+        HeroListComponent
+    ],
+    exports: [
+        HeroDetailComponent,
+        HeroListComponent
+    ],
+    providers: [HeroService]
+})
+export class HeroModule { }
